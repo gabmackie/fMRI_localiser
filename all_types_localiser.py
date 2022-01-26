b303a71b69d6fbad23afb7600add2a187453f14c
Gabriel Mackie <gabrielmackie@hotmail.com>
2022-01-26 11:50:21 +0000

    Did some testing at fullscreen, added timing info. All seems okay, need to check if certain settings are right for the experiment

--- all_types_localiser.py
+++ all_types_localiser.py
@@ -3,25 +3,37 @@ import os
 from glob import glob
 import random
 
+# Set escape key
 escape_key = 'escape'
 
+
+# Get current folder
 cwd = os.getcwd()
 
+# Get all the folders in the stimuli folder
 folders = glob(os.path.join(cwd, 'stimuli', '*'))
 print(folders)
 
+# Add every movie file in those folders to our stimuli dict, using the folders as keys
 all_files = {}
+
 for folder in folders:
     files = glob(os.path.join(folder, '*.mov'))
+
     all_files[folder] = files
 
+# Create our window
 win = visual.Window([1024, 768], units="pix",
-                    fullscr=False, allowGUI=True,
+                    fullscr=True, allowGUI=False,
                     color=(0, 0, 0))
+
+win.mouseVisible = False
 
 # Create a cross stimulus
 fix = visual.TextStim(win, text='+')
 
+
+# Create our stimuli
 all_stimuli = []
 
 # We want two sets, each containing one block of each type of stimuli
@@ -37,9 +49,11 @@ for i in range(2):
 
         # Now generate movie stimuli from the 6 files we chose
         block = []
+
         for file in chosen_files:
             stim = visual.MovieStim3(
                 win, filename=file, noAudio=True, units='norm', size=(2, 2))
+
             block.append(stim)  # Add the stimuli to the block
 
         set.append(block)  # And the block to the set
@@ -47,7 +61,10 @@ for i in range(2):
 
     all_stimuli.append(set)  # And the set to the all stimuli list
 
+
 # Add the wait until 5 is pressed?
+
+timing = win.flip()
 
 # Run the experiment
 for set in all_stimuli:
@@ -75,6 +92,9 @@ for set in all_stimuli:
 fix.draw()
 win.flip()
 core.wait(18)
+
+timing2 = win.flip()
+print(f'The length of time the experiment took is {timing2 - timing}')
 
 win.close()
 core.quit()
--- face_scene_localiser.py
+++ face_scene_localiser.py
@@ -1,22 +1,24 @@
-from turtle import st
 from psychopy import core, visual, event
 import os
 from glob import glob
 import random
 
+# Set the escape key
 escape_key = 'escape'
 
+# Get the current folder
 cwd = os.getcwd()
 
-# Specify the folders we want
+# Specify the folders we want as stimuli
 face_folder = os.path.join(cwd, 'stimuli', 'Faces')
-scene_folder = os.path.join(cwd, 'stimuli', 'Scenes')
+scene_folder = os.path.join(cwd, 'stimuli', 'Objects')
 
+# Create our window
 win = visual.Window([1024, 768], units="pix",
-                    fullscr=False, allowGUI=True,
+                    fullscr=True, allowGUI=False,
                     color=(0, 0, 0))
 
-#win.mouseVisible = False
+win.mouseVisible = False
 
 # Load in stimuli files
 face_files = glob(os.path.join(face_folder, '*.mov'))
@@ -26,6 +28,7 @@ all_files = [face_files, scene_files]
 
 # Generate stimuli from files
 all_stimuli = []
+
 # There should be 5 blocks of each type of stimuli
 for i in range(5):
 
@@ -34,19 +37,24 @@ for i in range(5):
         chosen_files = random.choices(file_type, k=6)
 
         block = []
+
         # Create a movie stimulus for each file
         for file in chosen_files:
             stim = visual.MovieStim3(
                 win, filename=file, noAudio=True, units='norm', size=(2, 2))
+
             block.append(stim)  # Add it to the block
 
         all_stimuli.append(block)  # Add the block to all_stimuli
+
+timing = win.flip()
 
 # Experiment begins here
 # Run each block at a time
 for block in all_stimuli:
     for mov in block:
         # Draw each stimuli
+
         while mov.status != visual.FINISHED:
             mov.draw()
             win.flip()
@@ -58,6 +66,10 @@ for block in all_stimuli:
             print('User requested to quit, ending experiment')
             win.close()
             core.quit()
+
+
+timing2 = win.flip()
+print(f'The length of time the experiment took is {timing2 - timing}')
 
 win.close()
 core.quit()
